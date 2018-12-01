2814597ddaa8a286e2424892cc4fb5d177024b71
Aria <aria@ar1as.space>
2018-12-01 22:56:15 +1100

    init

---
+++ config/config.exs
@@ -0,0 +1,30 @@
+# This file is responsible for configuring your application
+# and its dependencies with the aid of the Mix.Config module.
+#
+# This configuration file is loaded before any dependency and
+# is restricted to this project.
+
+# General application configuration
+use Mix.Config
+
+config :medium_graphql_api,
+  ecto_repos: [MediumGraphqlApi.Repo]
+
+# Configures the endpoint
+config :medium_graphql_api, MediumGraphqlApiWeb.Endpoint,
+  url: [host: "localhost"],
+  secret_key_base: "O1L8GroeGV1r/+TGiU86uhP18x8MXUgjGArRxNL9bjKYdhnQH6QX9JUytht2sX6E",
+  render_errors: [view: MediumGraphqlApiWeb.ErrorView, accepts: ~w(json)],
+  pubsub: [name: MediumGraphqlApi.PubSub, adapter: Phoenix.PubSub.PG2]
+
+# Configures Elixir's Logger
+config :logger, :console,
+  format: "$time $metadata[$level] $message\n",
+  metadata: [:request_id]
+
+# Use Jason for JSON parsing in Phoenix
+config :phoenix, :json_library, Jason
+
+# Import environment specific config. This must remain at the bottom
+# of this file so it overrides the configuration defined above.
+import_config "#{Mix.env()}.exs"
