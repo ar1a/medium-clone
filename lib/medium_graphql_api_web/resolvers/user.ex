21dbe6369c5f00dac4331452b914cf72a4082ea4
Aria <aria@ar1as.space>
2018-12-02 00:50:32 +1100

    add login resolver

--- lib/medium_graphql_api_web/resolvers/user.ex
+++ lib/medium_graphql_api_web/resolvers/user.ex
@@ -7,4 +7,11 @@ defmodule MediumGraphqlApiWeb.Resolvers.User do
   def register(_, %{input: input}, _) do
     MediumGraphqlApi.Accounts.create_user(input)
   end
+
+  def login(_, %{input: input}, _) do
+    with {:ok, user} <- MediumGraphqlApi.Accounts.Session.authenticate(input),
+         {:ok, token, _} <- MediumGraphqlApi.Guardian.encode_and_sign(user) do
+      {:ok, %{token: token, user: user}}
+    end
+  end
 end
